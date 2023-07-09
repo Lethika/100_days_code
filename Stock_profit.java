903d1da28576f05dad9a52ac9b14e863f9df8b43
Lethika.R <106001986+Lethika@users.noreply.github.com>
2023-07-09 21:08:03 +0530

    Create Stock_profit.java

---
+++ Stock_profit.java
@@ -0,0 +1,26 @@
+package p100days_code;
+
+
+
+public class stock_buy {
+
+
+
+    public static void main(String[] args) {
+    int arr[] = {7,1,5,3,6,4};
+
+    int maxPro = maxProfit(arr);
+    System.out.println("Max profit is: " + maxPro);
+
+    }
+    static int maxProfit(int[] arr) {
+    int maxPro = 0;
+    int minPrice = Integer.MAX_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+        minPrice = Math.min(minPrice, arr[i]);
+        maxPro = Math.max(maxPro, arr[i] - minPrice);
+    }
+    return maxPro;
+    }
+}
+
