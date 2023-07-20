0e55f07f333d2b3d002f6f55f89e0a8f514aa201
Lethika.R <106001986+Lethika@users.noreply.github.com>
2023-07-20 22:09:37 +0530

    Create Single_elementinarray.java

---
+++ Single_elementinarray.java
@@ -0,0 +1,21 @@
+package p100days_code;
+
+public class Single_element {
+	public static int getSingleElement(int []arr) {
+        //size of the array:
+        int n = arr.length;
+
+        // XOR all the elements:
+        int xorr = 0;
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ arr[i];
+        }
+        return xorr;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+}
+}
