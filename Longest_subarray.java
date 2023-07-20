4fd5592b3c8590e5d28930bdd6081b8c62b80a8c
Lethika.R <106001986+Lethika@users.noreply.github.com>
2023-07-20 22:06:32 +0530

    Create Longest_subarray.java

---
+++ Longest_subarray.java
@@ -0,0 +1,27 @@
+package p100days_code;
+
+public class Longest_Subarray {
+	public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { // starting index
+            long s = 0; // Sum variable
+            for (int j = i; j < n; j++) { 
+               
+                s += a[j];
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
