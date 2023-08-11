3d094a71782670c3bd0981bfe85faa15aa5f5e0a
Lethika.R <106001986+Lethika@users.noreply.github.com>
2023-08-11 21:56:23 +0530

    Create Palindrome_String.java

---
+++ Palindrome_String.java
@@ -0,0 +1,29 @@
+package p100days_code;
+
+import java.util.Scanner;
+
+public class Pallindrome_string {
+
+	 public static void main(String args[])
+	   {
+	      String str, rev = "";
+	      Scanner sc = new Scanner(System.in);
+	 
+	      System.out.println("Enter a string:");
+	      str = sc.nextLine();
+	 
+	      int length = str.length();
+	 
+	      for ( int i = length - 1; i >= 0; i-- )
+	         rev = rev + str.charAt(i);
+	 
+	      if (str.equals(rev))
+	         System.out.println(str+" is a palindrome");
+	      else
+	         System.out.println(str+" is not a palindrome");
+	 
+	   }
+
+	}
+
+
